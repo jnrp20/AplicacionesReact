acba52b32b6f1edcaed03c2f75d442478f55f0be
jnrp20 <jromeropineda443@gmail.com>
2023-09-26 16:29:09 -0500

    mejoras visuales del header

--- primera-app-react/src/COMPONENTS/Component1.js
+++ primera-app-react/src/COMPONENTS/Component1.js
@@ -4,55 +4,86 @@ import ReactDOM from "react-dom";
 
 import bombilla from "../image/bombilla.png";
 import bombillaOn from "../image/bombillaOn.png";
+import logo from "../image/patitaslogo.png";
 // las imagenes se tienen que importar no se puede poner la ruta directamente como en un html normal
 
 export default function Component1() {
+
+    /*  estas ref ayudan para poder editar el dom en las funciones de javaScript, se pone la referencia en los tags ej: en los div y eso viene a remplazar el id */
+    let estadoBombillo = false;
     let refBombillo = useRef(),
         refbody = useRef(),
-        refletras = useRef();
-    let estadoBombillo = false;
+        refletras = useRef(),
+        refregistrarse = useRef();
 
     return (
         <div className="padre" ref={refbody}>
             <header ref={refletras}>
-                <img className="logo"
-                    width="8%"
-                    height="auto"
-                    src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png"
-                    alt=""
-                />
-                <h1>Patitas APP</h1>
+
+
+
+                <div className="logodiv">
+
+                    <img 
+
+                        id="bombillo"
+                        src={logo}
+                        alt=""
+                        width="10%"
+
+                    />
+
+
+
+                    <h1>patitas app</h1>
+                </div>
+
                 <div className="btn-sesion">
-                    <img
+                    <img className="img-bombillo"
                         onClick={prueba}
                         id="bombillo"
                         src={bombillaOn}
-                        alt=""
-                        width="15%"
+                       
+                        width="12%"
+                       
                         ref={refBombillo}
                     />
+
+
                     {/* en este img el id bombillo queda obsoleto y se remplaza con las referencias */}
 
                     <h4>Iniciar sesión</h4>
-                    <h4>Registrarse</h4>
+
+
+
+                    <div className="btn-registrarse" ref={refregistrarse}>
+
+
+                        <h4>Registrarse</h4>
+
+                    </div>
                 </div>
             </header>
-            
+
         </div>
     );
 
     function prueba() {
         if (estadoBombillo == false) {
+
+            /* no olvidar el .current */
+
             refbody.current.style.backgroundColor = "rgb(44, 43, 43)";
-            refletras.current.style.color="#c1c1c1";
-            refletras.current.style.backgroundColor="#0078e3";
+            refregistrarse.current.style.backgroundColor = "#917cc5";
+            refletras.current.style.backgroundColor = "#0078e3";
             refBombillo.current.src = bombilla;
             console.log("se apagó el bombillo");
             estadoBombillo = true;
         } else {
             refbody.current.style.backgroundColor = "white";
-            refletras.current.style.color="white";
-            refletras.current.style.backgroundColor="rgb(66, 147, 212)";
+            refregistrarse.current.style.backgroundColor = "chocolate";
+            refletras.current.style.color = "white";
+            refletras.current.style.backgroundColor = "#034A68";
             refBombillo.current.src = bombillaOn;
             console.log("se prendió el bombillo");
             estadoBombillo = false;
